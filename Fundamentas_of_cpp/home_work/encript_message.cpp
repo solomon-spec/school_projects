120a2b182fd70c733770d8c2aed14e7a0c0f7c42
solomon-spec <70583058+solomon-spec@users.noreply.github.com>
2023-03-31 00:45:00 -0700

    Update encript_message.cpp

--- Fundamentas_of_cpp/home_work/encript_message.cpp
+++ Fundamentas_of_cpp/home_work/encript_message.cpp
@@ -2,10 +2,7 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-int main() {
-    cout<<"enter a 4 digit number"<<endl;
-    int number;
-    cin>>number;
+int encrypt(int number){
     int dummy = number;
     int ans = 0;
     for(int i = 3; i>=0; i--){
@@ -19,22 +16,48 @@ int main() {
         
         
     }
-    cout<<"encripted is "<<ans<<endl;
+    cout<<"encrypted is "<<ans<<endl;
     dummy = ans;
-    ans = 0;
+    return 0;
+}
+
+
+int decrypt(int number){
+    int dummy = number;
+    int ans = 0;
     for(int i = 3; i>=0; i--){
     
         int cur_dig = dummy/pow(10,i);
         dummy -= cur_dig*pow(10,i);
+        
         if (cur_dig >= 7) cur_dig = cur_dig - 7;
         else cur_dig += 3;
+        
         cur_dig = cur_dig*pow(10,i + 2);
         if (cur_dig >= 10000) cur_dig /= 10000;
-        cout<<cur_dig<<" "<< dummy<<endl;
         ans += cur_dig;
         
     }
-    cout<<"decripted is "<<ans;
+    cout<<"decrypted is "<<ans;
+    return 0;
+}
+
+int main() {
+    cout<<"enter a 4 digit number"<<endl;
+    int number;
+    cin>>number;
+    int choose;
+    cout<<"1 for encrypt 2 for decrept"<<endl;
+    cin>>choose;
+    if(choose == 1){
+        encrypt(number);
+    }
+    else{
+        decrypt(number);
+    }
+    
+    
+   
     
     
 }
