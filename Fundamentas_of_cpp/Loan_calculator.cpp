e95e0c11ed3749af58efca865653e2af76c3b5b7
Solomon <70583058+solomon-spec@users.noreply.github.com>
2023-04-08 18:54:44 +0300

    Update Loan_calculator.cpp

--- Fundamentas_of_cpp/Loan_calculator.cpp
+++ Fundamentas_of_cpp/Loan_calculator.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main() {
    string def_program = "This program takes loan amount and interest rate as an input and calculates the monthly payment and the remening loan for each month";
-   
-   cout<< def_program<<endl;
-   cout<<"Enter the loan amount"<<endl;
+   cout<< def_program<<"\n"<<endl;
+   program:
+   cout<<"Enter the loan amount\t";
    float loan;
    cin>>loan;
   
-   cout<<"Inter the interest rate"<<endl;
+   cout<<"Inter the interest rate\t";
    float interest;
    cin>>interest;
    interest /= 100;
@@ -19,7 +19,7 @@ int main() {
    float this_month_interest;
    float remaining_month_payment;
    int month = 1;
-   cout<<"month\t\tmonthly payment\t\tremaining loan"<<endl;
+   cout<<"month\t monthly payment\t remaining loan"<<endl;
    while (loan > 0){
        
        this_month_interest = (loan * interest)/12;
@@ -33,11 +33,16 @@ int main() {
            loan = 0;
        }
        
-        cout<<"month "<<month<<"\t\t";
-        cout<<monthly_payment<<" BIRR"<<"\t\t";
+        cout<<"month "<<month<<" \t";
+        cout<<monthly_payment<<" BIRR\t\t\t";
         cout<<loan<<" BIRR"<<endl  ;
         month += 1;
        
    }
+  char choice;
+  cout<<"\nDo You Want to Exit (y/n)\t";
+  cin>>choice;
+  if(choice != 'Y' && choice != 'y')
+    goto program;
    
 }
