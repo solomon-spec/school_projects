3983dd82e1392cf25841d02a99e3a32371611145
Solomon <70583058+solomon-spec@users.noreply.github.com>
2023-04-08 19:05:07 +0300

    Update print_numbers_without_some_digits.cpp

--- Fundamentas_of_cpp/print_numbers_without_some_digits.cpp
+++ Fundamentas_of_cpp/print_numbers_without_some_digits.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include<cmath>
+#include<string>
+
 using namespace std;
 int main() {
-    
+    string def_program = "This program takes a degree from 0 - 999 ans returns the closest degree that doesnt have 1,4 & 7 in it's digit";
+    cout<<def_program<<"\n\n";
+    program:
    int temprature;
+   cout<<"Enter temprature\t";
    cin >> temprature;
    int dummy_temp = temprature;
    int ans[2];
@@ -15,10 +20,16 @@ int main() {
            ans[0] = (int(temprature / pow(10,i)))*pow(10,i) + pow(10,i);
            ans[1] = (int(temprature / pow(10,i)))*pow(10,i) - 1;
            cout<<ans[0]<<" "<<ans[1];
-           return 0;
+           goto close_program;
        }
        dummy_temp -= dig*pow(10,i);
    }
     cout<<temprature;
+    char choice;
+    close_program:
+    cout<<"\nDo You Want to Exit (y/n)\t";
+    cin>>choice;
+    if(choice != 'Y' && choice != 'y')
+    goto program;
     return 0;
-}  
+} 
